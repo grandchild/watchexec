decd3aea1d9e95ce55dfa057ec08917238df6a4f
jakob <grandchild@gmx.net>
2018-03-12 10:49:16 +0100

    Add --initial flag to run command once immediately

--- watchexec.py
+++ watchexec.py
@@ -2,6 +2,7 @@
 import argparse
 import subprocess as proc
 import os.path as path
+import os
 
 
 parser = argparse.ArgumentParser(
@@ -11,18 +12,21 @@ parser.add_argument("commands", nargs="*", default=["echo %f"],
 					help="Specify the command(s) to run on the changed file. Use %%f to refer to the file inside the command.")
 parser.add_argument("-p", "--path", default=".", type=str, help="Directory or file to watch.")
 parser.add_argument("-o", "--once", default=False, action="store_true", help="Run the task only on the first change. Then exit.")
+parser.add_argument("-i", "--initial", default=False, action="store_true", help="Run the task once initially, regardless whether there are any changes.")
 parser.add_argument("-d", "--debug", default=False, action="store_true", help="Show debug info.")
 args = parser.parse_args()
 
 
-
+cmd = "; ".join([c.replace("%f", "${filename}") for c in args.commands])
 notify_cmd = "inotifywait --quiet --recursive --exclude '\\.git' --event close_write,moved_to,create {dir} | while read -r directory events filename; do {cmd}; done".format(
 	dir = path.abspath(path.join(".", args.path)) if not args.path.startswith("/") else args.path,
-	cmd = "; ".join([c.replace("%f", "${filename}") for c in args.commands]))
+	cmd = cmd)
 
 if args.debug:
 	print(notify_cmd)
 try:
+	if args.initial:
+		os.system(cmd)
 	if args.once:
 		proc.run(notify_cmd, shell=True)
 	else:
