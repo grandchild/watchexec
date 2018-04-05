955ffa9b139f2eba66b092576c87c541d69b74c2
jakob <grandchild@gmx.net>
2018-04-05 15:32:05 +0200

    Add --ignore-dirs arg to customize ignored dirs

--- watchexec.py
+++ watchexec.py
@@ -3,6 +3,10 @@ import argparse
 import subprocess as proc
 import os.path as path
 import os
+import re
+
+
+DEFAULT_IGNORE_DIRS = [".git", ".svn"]
 
 
 parser = argparse.ArgumentParser(
@@ -13,12 +17,14 @@ parser.add_argument("commands", nargs="*", default=["echo %f"],
 parser.add_argument("-p", "--path", default=".", type=str, help="Directory or file to watch.")
 parser.add_argument("-o", "--once", default=False, action="store_true", help="Run the task only on the first change. Then exit.")
 parser.add_argument("-i", "--initial", default=False, action="store_true", help="Run the task once initially, regardless whether there are any changes.")
+parser.add_argument("-I", "--ignore-dirs", default=DEFAULT_IGNORE_DIRS, nargs="*", metavar="DIR", help="List of directories which are not watched.")
 parser.add_argument("-d", "--debug", default=False, action="store_true", help="Show debug info.")
 args = parser.parse_args()
 
 
 cmd = "; ".join([c.replace("%f", "${filename}") for c in args.commands])
-notify_cmd = "inotifywait --quiet --recursive --exclude '\\.git' --event close_write,moved_to,create {dir} | while read -r directory events filename; do {cmd}; done".format(
+notify_cmd = "inotifywait --quiet --recursive {excludes} --event close_write,moved_to,create {dir} | while read -r directory events filename; do {cmd}; done".format(
+	excludes = "--exclude '" + ("|".join([re.escape(d) for d in args.ignore_dirs])) + "'" if args.ignore_dirs else "",
 	dir = path.abspath(path.join(".", args.path)) if not args.path.startswith("/") else args.path,
 	cmd = cmd)
 
