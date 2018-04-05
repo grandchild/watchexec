58d4420e8f89a6f4e323fac928596c0ef846825b
jakob <grandchild@gmx.net>
2018-04-05 15:32:44 +0200

    Convert indentation to spaces

--- watchexec.py
+++ watchexec.py
@@ -10,10 +10,10 @@ DEFAULT_IGNORE_DIRS = [".git", ".svn"]
 
 
 parser = argparse.ArgumentParser(
-	description="Run command whenever files in the path change.",
-	formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    description="Run command whenever files in the path change.",
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("commands", nargs="*", default=["echo %f"],
-					help="Specify the command(s) to run on the changed file. Use %%f to refer to the file inside the command.")
+                    help="Specify the command(s) to run on the changed file. Use %%f to refer to the file inside the command.")
 parser.add_argument("-p", "--path", default=".", type=str, help="Directory or file to watch.")
 parser.add_argument("-o", "--once", default=False, action="store_true", help="Run the task only on the first change. Then exit.")
 parser.add_argument("-i", "--initial", default=False, action="store_true", help="Run the task once initially, regardless whether there are any changes.")
@@ -24,19 +24,19 @@ args = parser.parse_args()
 
 cmd = "; ".join([c.replace("%f", "${filename}") for c in args.commands])
 notify_cmd = "inotifywait --quiet --recursive {excludes} --event close_write,moved_to,create {dir} | while read -r directory events filename; do {cmd}; done".format(
-	excludes = "--exclude '" + ("|".join([re.escape(d) for d in args.ignore_dirs])) + "'" if args.ignore_dirs else "",
-	dir = path.abspath(path.join(".", args.path)) if not args.path.startswith("/") else args.path,
-	cmd = cmd)
+    excludes = "--exclude '" + ("|".join([re.escape(d) for d in args.ignore_dirs])) + "'" if args.ignore_dirs else "",
+    dir = path.abspath(path.join(".", args.path)) if not args.path.startswith("/") else args.path,
+    cmd = cmd)
 
 if args.debug:
-	print(notify_cmd)
+    print(notify_cmd)
 try:
-	if args.initial:
-		os.system(cmd)
-	if args.once:
-		proc.run(notify_cmd, shell=True)
-	else:
-		while True:
-			proc.run(notify_cmd, shell=True)
+    if args.initial:
+        os.system(cmd)
+    if args.once:
+        proc.run(notify_cmd, shell=True)
+    else:
+        while True:
+            proc.run(notify_cmd, shell=True)
 except KeyboardInterrupt:
-	print()
+    print()
